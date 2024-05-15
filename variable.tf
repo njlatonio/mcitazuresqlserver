5ebd169f58746c10f0b3587dd16b7745513852c6
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-15 18:36:46 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "subscription_id"{
+  type=string
+}
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+variable "administrator_login"{
+  type=string
+}
+variable "administrator_login_password"{
+  type=string
+}
