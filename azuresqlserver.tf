0f265f9a36247a5c6a380ccb583214ac9c05e75c
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-15 18:35:04 -0400

    Create azuresqlserver.tf

---
+++ azuresqlserver.tf
@@ -0,0 +1,23 @@
+resource "azurerm_resource_group" "databaserg" {
+  name     = "database-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_mssql_server" "azuresqlserver" {
+  name                         = "mssqlserver"
+  resource_group_name          = azurerm_resource_group.databaserg.name
+  location                     = azurerm_resource_group.databaserg.location
+  version                      = "12.0"
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
+  minimum_tls_version          = "1.2"
+
+  azuread_administrator {
+    login_username = "AzureAD Admin"
+    object_id      = "00000000-0000-0000-0000-000000000000"
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
