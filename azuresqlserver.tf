f30e8916957db9591504ea6f380cf74f24b426e9
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-15 18:41:52 -0400

    Update azuresqlserver.tf

--- azuresqlserver.tf
+++ azuresqlserver.tf
@@ -4,13 +4,13 @@ resource "azurerm_resource_group" "databaserg" {
 }
 
 resource "azurerm_mssql_server" "azuresqlserver" {
-  name                         = "mssqlserver"
+  name                         = var.mysqlname
   resource_group_name          = azurerm_resource_group.databaserg.name
   location                     = azurerm_resource_group.databaserg.location
-  version                      = "12.0"
+  version                      = var.version
   administrator_login          = var.administrator_login
   administrator_login_password = var.administrator_login_password
-  minimum_tls_version          = "1.2"
+  minimum_tls_version          = var.minimum_tls_version
 
   azuread_administrator {
     login_username = "AzureAD Admin"
